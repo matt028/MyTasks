fc3a59300dd0eb11f8606305739999222be93036
Matt Sutton <mattdsutton28@gmail.com>
2018-05-20 20:07:20 -0400

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Matthew Sutton on 5/20/18.
+//  Copyright © 2018 Matthew Sutton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
